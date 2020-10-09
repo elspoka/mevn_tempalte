096eab948e0f9babe3d4f15c03cc1cf2a2a1680f
Kalantzis <ikalantzis@deloitte.gr>
2020-10-09 18:11:55 +0300

    First test

--- server/config/prod.js
+++ server/config/prod.js
@@ -7,10 +7,13 @@ module.exports = {
 	},
 
 	db: {
-		uri: process.env.MONGO_URI || "mongodb://localhost/" + pkg.config.dbName,
-		options: {
-			user: process.env.MONGO_USERNAME || "",
-			pass: process.env.MONGO_PASSWORD || ""
-		}
+		uri: 'mongodb://u4my0bcgukkjwvdpjplg:SoYdPuvdnn6DeKjwPnj6@bb4tktwup1pzjcz-mongodb.services.clever-cloud.com:27017/bb4tktwup1pzjcz?authSource=bb4tktwup1pzjcz&readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+		// uri: process.env.MONGO_URI || "mongodb://localhost/" + pkg.config.dbName,
+		// options: {
+		// 	user: process.env.MONGO_USERNAME || "",
+		// 	pass: process.env.MONGO_PASSWORD || ""
+		// }
 	}
 };
+
+
